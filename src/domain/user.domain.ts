2db11d5d0fa5edb1a679b61080d0eafb1d09bd00
SecondPhantom <second2phantom@gmail.com>
2024-02-09 21:03:26 +0900

    feat: add user domain

--- src/domain/user.domain.ts
+++ src/domain/user.domain.ts
@@ -1,3 +1,65 @@
+import { users } from "../schema/schema";
+import { v4 as uuidv4 } from "uuid";
+
+export type UserEntity = typeof users.$inferInsert;
+
+export type RepoCreateUserDto = Pick<
+  Required<UserEntity>,
+  "uuid" | "email" | "hashKey" | "profileName" | "extra"
+>;
+
 export class UserDomain {
-  constructor(...any: any[]) {}
+  private id: number | undefined;
+  private uuid: string;
+  private role: number;
+  private email: string;
+  private hashKey: string;
+  private emailVerified: boolean;
+  private profileName: string;
+  private profileImage: string | null;
+  private extra: any;
+  private createdAt: Date | undefined;
+  private updatedAt: Date | undefined;
+
+  private ROLE = {
+    admin: 0,
+    user: 1,
+  };
+
+  constructor({
+    id,
+    uuid,
+    role,
+    email,
+    hashKey,
+    emailVerified,
+    profileName,
+    profileImage,
+    extra,
+    createdAt,
+    updatedAt,
+  }: Omit<UserEntity, "uuid" | "extra"> &
+    Partial<Pick<UserEntity, "uuid" | "extra">>) {
+    this.id = id;
+    this.uuid = uuid ? uuid : uuidv4();
+    this.role = role ? role : this.ROLE["user"];
+    this.email = email;
+    this.hashKey = hashKey;
+    this.emailVerified = emailVerified === undefined ? false : emailVerified;
+    this.profileName = profileName;
+    this.profileImage = profileImage ? profileImage : null;
+    this.extra = extra ? extra : {};
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  getCreateUserDto = (): RepoCreateUserDto => {
+    return {
+      uuid: this.uuid,
+      email: this.email,
+      hashKey: this.hashKey,
+      extra: this.extra,
+      profileName: this.profileName,
+    };
+  };
 }
