b7b55b5babba6fdf6b69a05037c02f11e64ce207
SecondPhantom <second2phantom@gmail.com>
2024-02-16 22:44:16 +0900

    feat: add ControllerReponse's header property

--- src/dto/response.ts
+++ src/dto/response.ts
@@ -1,5 +1,6 @@
 export class ControllerResponse<T = any> {
   private code: number;
+  private headers: { [key in string]: string };
   private payload: {
     success: boolean;
     message: string;
@@ -7,9 +8,11 @@ export class ControllerResponse<T = any> {
   };
   constructor({
     code,
+    headers,
     payload,
   }: {
     code: number;
+    headers?: { [key in string]: string };
     payload: {
       success: boolean;
       message?: string;
@@ -19,12 +22,14 @@ export class ControllerResponse<T = any> {
     this.code = code;
     payload.message ??= "";
     this.payload = payload as any;
+    this.headers = headers ? headers : {};
   }
 
   getResponse = () => {
     return {
       code: this.code,
       payload: this.payload,
+      headers: this.headers,
     };
   };
 }
