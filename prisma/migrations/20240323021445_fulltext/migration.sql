b0aab13ab8d4061f7d14bd5a744fd501a19d736c
SecondPhantom <second2phantom@gmail.com>
2024-04-23 21:20:28 +0900

    feat: updated tale schema

--- prisma/migrations/20240323021445_fulltext/migration.sql
+++ prisma/migrations/20240323021445_fulltext/migration.sql
@@ -32,6 +32,7 @@ CREATE TABLE "Channels" (
 -- CreateTable
 CREATE TABLE "Courses" (
     "id" BIGSERIAL NOT NULL,
+		"version" INTEGER NOT NULL DEFAULT 1,
     "video_id" VARCHAR(50) NOT NULL,
     "channel_id" VARCHAR(50) NOT NULL,
     "category_id" INTEGER NOT NULL,
@@ -51,48 +52,6 @@ CREATE TABLE "Courses" (
 
     CONSTRAINT "Courses_pkey" PRIMARY KEY ("id")
 );
-
--- CreateTable
-CREATE TABLE "Enrolls" (
-    "user_id" BIGINT NOT NULL,
-    "course_id" BIGINT NOT NULL,
-    "chapter_progress" JSONB NOT NULL,
-    "total_progress" REAL NOT NULL,
-    "recent_progress" JSONB NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
-
-    CONSTRAINT "Enrolls_pkey" PRIMARY KEY ("user_id","course_id")
-);
-
--- CreateTable
-CREATE TABLE "Categories" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR(100) NOT NULL,
-    "parent_id" INTEGER NOT NULL,
-
-    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Roles" (
-    "id" INTEGER NOT NULL,
-    "name" VARCHAR(100) NOT NULL,
-
-    CONSTRAINT "Roles_pkey" PRIMARY KEY ("id")
-);
-
--- CreateIndex
-CREATE INDEX "Users_role_id_idx" ON "Users"("role_id");
-
--- CreateIndex
-CREATE INDEX "Users_created_at_idx" ON "Users"("created_at" DESC);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
-
--- CreateIndex
-CREATE INDEX "Channels_name_tsvector_idx" ON "Channels" USING GIN ("name_tsvector");;
 
 -- CreateIndex
 CREATE INDEX "Courses_channel_id_idx" ON "Courses"("channel_id");
@@ -121,6 +80,30 @@ CREATE INDEX "Courses_title_tsvector_idx" ON "Courses" USING GIN ("title_tsvecto
 -- CreateIndex
 CREATE UNIQUE INDEX "Courses_video_id_key" ON "Courses"("video_id");
 
+-- CreateTrigger
+CREATE TRIGGER course_title_vector_update BEFORE INSERT OR UPDATE
+ON "Courses" FOR EACH ROW EXECUTE FUNCTION
+tsvector_update_trigger(title_tsvector, 'pg_catalog.simple', title);
+
+-- SetTrigger
+CREATE TRIGGER table_update
+BEFORE UPDATE ON "Courses"
+FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+-- CreateTable
+CREATE TABLE "Enrolls" (
+    "user_id" BIGINT NOT NULL,
+    "course_id" BIGINT NOT NULL,
+		"version" INTEGER NOT NULL,
+    "chapter_progress" JSONB NOT NULL,
+    "total_progress" REAL NOT NULL,
+    "recent_progress" JSONB NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "Enrolls_pkey" PRIMARY KEY ("user_id","course_id")
+);
+
 -- CreateIndex
 CREATE INDEX "Enrolls_course_id_idx" ON "Enrolls"("course_id");
 
@@ -131,16 +114,110 @@ CREATE INDEX "Enrolls_user_id_created_at_idx" ON "Enrolls"("user_id", "created_a
 CREATE INDEX "Enrolls_user_id_updated_at_idx" ON "Enrolls"("user_id", "updated_at" DESC);
 
 -- CreateIndex
+CREATE INDEX "Enrolls_updated_at_idx" ON "Enrolls"("updated_at" DESC);
+
+-- SetTrigger
+CREATE TRIGGER table_update
+BEFORE UPDATE ON "Enrolls"
+FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+-- CreateTable
+CREATE TABLE "Categories" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "parent_id" INTEGER NOT NULL,
+		"created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+		"updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Categories_created_at_idx" ON "Categories"("created_at" DESC);
+
+-- CreateIndex
+CREATE INDEX "Categories_updated_at_idx" ON "Categories"("updated_at" DESC);
+
+-- SetTrigger
+CREATE TRIGGER table_update
+BEFORE UPDATE ON "Categories"
+FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+-- CreateTable
+CREATE TABLE "Roles" (
+    "id" INTEGER NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+		"created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+		"updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "Roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Roles_created_at_idx" ON "Roles"("created_at" DESC);
+
+-- CreateIndex
+CREATE INDEX "Roles_updated_at_idx" ON "Roles"("updated_at" DESC);
+
+-- SetTrigger
+CREATE TRIGGER table_update
+BEFORE UPDATE ON "Roles"
+FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+-- CreateTable
+CREATE TABLE "Admins" (
+    "id" BIGSERIAL NOT NULL,
+    "email" VARCHAR(320) NOT NULL,
+		"role_id" INTEGER NOT NULL DEFAULT 1,
+    "otp_code" VARCHAR(10),
+		"otp_expiration_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "profile_name" VARCHAR(100) NOT NULL,
+    "profile_image" VARCHAR(300),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "Admins_pkey" PRIMARY KEY ("id")
+);
+
+
+-- CreateIndex
+CREATE INDEX "Users_role_id_idx" ON "Users"("role_id");
+
+-- CreateIndex
+CREATE INDEX "Users_created_at_idx" ON "Users"("created_at" DESC);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE INDEX "Channels_name_tsvector_idx" ON "Channels" USING GIN ("name_tsvector");;
+
+
+
+
+
+
+
+-- CreateIndex
 CREATE INDEX "Categories_parent_id_idx" ON "Categories"("parent_id");
 
-CREATE TRIGGER course_title_vector_update BEFORE INSERT OR UPDATE
-ON "Courses" FOR EACH ROW EXECUTE FUNCTION
-tsvector_update_trigger(title_tsvector, 'pg_catalog.simple', title);
+-- CreateIndex
+CREATE UNIQUE INDEX "Admins_email_key" ON "Admins"("email");
 
+-- CreateIndex
+CREATE INDEX "Admins_role_id_idx" ON "Admins"("role_id");
+
+-- CreateIndex
+CREATE INDEX "Admins_created_at_idx" ON "Admins"("created_at" DESC);
+
+
+
+-- CreateTrigger
 CREATE TRIGGER channel_name_vector_update BEFORE INSERT OR UPDATE
 ON "Channels" FOR EACH ROW EXECUTE FUNCTION
 tsvector_update_trigger(name_tsvector, 'pg_catalog.simple', 'name');
 
+-- CreateTrigger
 CREATE OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS
 $set_updated_at$
 BEGIN
@@ -151,21 +228,131 @@ BEGIN
 END;
 $set_updated_at$ LANGUAGE plpgsql;
 
+-- SetTrigger
 CREATE TRIGGER table_update
 BEFORE UPDATE ON "Users"
 FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
 
+-- SetTrigger
 CREATE TRIGGER table_update
 BEFORE UPDATE ON "Channels"
 FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
 
+
+
+
+
+
+-- CreateTable
+CREATE TABLE "AdminSessions" (
+		"id" BIGSERIAL NOT NULL,
+    "session_key" VARCHAR(50) NOT NULL,
+		"admin_id" BIGINT NOT NULL,
+		"data" JSONB NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "AdminSessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "AdminSessions_admin_id_idx" ON "AdminSessions"("admin_id");
+
+-- CreateIndex
+CREATE INDEX "AdminSessions_created_at_idx" ON "AdminSessions"("created_at" DESC);
+
+-- CreateIndex
+CREATE INDEX "AdminSessions_updated_at_idx" ON "AdminSessions"("updated_at" DESC);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdminSessions_session_key_key" ON "AdminSessions"("session_key");
+
+-- SetTrigger
 CREATE TRIGGER table_update
-BEFORE UPDATE ON "Courses"
+BEFORE UPDATE ON "AdminSessions"
 FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
 
+
+-- CreateTable
+CREATE TABLE "Healths" (
+    "id" BIGSERIAL NOT NULL,
+		"version" BIGINT NOT NULL,
+		"data" JSONB NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "Healths_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Healths_version_idx" ON "Healths"("version");
+
+-- CreateIndex
+CREATE INDEX "Healths_created_at_idx" ON "Healths"("created_at" DESC);
+
+-- CreateTable
+CREATE TABLE "UserStats" (
+		"version" BIGINT NOT NULL,
+		"event_at" DATE NOT NULL,
+		"data" JSONB NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "UserStats_pkey" PRIMARY KEY ("version","event_at")
+);
+
+-- CreateIndex
+CREATE INDEX "UserStats_created_at_idx" ON "UserStats"("created_at" DESC);
+
+
+-- CreateTable
+CREATE TABLE "UserCredits" (
+    "user_id" BIGSERIAL NOT NULL,
+		"free_credits" INTEGER NOT NULL DEFAULT 0,
+		"purchased_credits" INTEGER NOT NULL DEFAULT 0,
+    "free_credit_received_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "purchased_credit_received_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT "UserCredits_pkey" PRIMARY KEY ("user_id")
+);
+
+-- SetTrigger
 CREATE TRIGGER table_update
-BEFORE UPDATE ON "Enrolls"
+BEFORE UPDATE ON "UserCredits"
 FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
 
+-- CreateTable
+CREATE TABLE "Notices" (
+    "id" BIGSERIAL NOT NULL,
+		"admin_id" BIGINT NOT NULL,
+		"title" VARCHAR(200) NOT NULL,
+		"content" VARCHAR(10000) NOT NULL,
+		"is_displayed_on" BOOLEAN NOT NULL DEFAULT false,
+    "display_start_date" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "display_end_date" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
 
+    CONSTRAINT "Notices_pkey" PRIMARY KEY ("id")
+);
 
+-- CreateIndex
+CREATE INDEX "Notices_admin_id_idx" ON "Notices"("admin_id");
+
+-- CreateIndex
+CREATE INDEX "Notices_is_displayed_on_idx" ON "Notices"("is_displayed_on");
+
+-- CreateIndex
+CREATE INDEX "Notices_display_start_date_idx" ON "Notices"("display_start_date" DESC);
+
+-- CreateIndex
+CREATE INDEX "Notices_display_end_date_idx" ON "Notices"("display_end_date" DESC);
+
+-- CreateIndex
+CREATE INDEX "Notices_created_at_idx" ON "Notices"("created_at" DESC);
+
+-- SetTrigger
+CREATE TRIGGER table_update
+BEFORE UPDATE ON "Notices"
+FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
