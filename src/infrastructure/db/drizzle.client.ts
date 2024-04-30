faeca1441ce2586f602bd17fcbce238cb0db1ef6
SecondPhantom <second2phantom@gmail.com>
2024-04-30 13:35:08 +0900

    feat: adding a usingTrx method at drizzleClient

--- src/infrastructure/db/drizzle.client.ts
+++ src/infrastructure/db/drizzle.client.ts
@@ -1,12 +1,23 @@
 import { Client } from "pg";
-import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+import {
+  NodePgDatabase,
+  NodePgQueryResultHKT,
+  drizzle,
+} from "drizzle-orm/node-postgres";
 
 import * as schema from "../../schema/schema";
 import { ENV } from "../../env";
+import { PgTransaction } from "drizzle-orm/pg-core";
+import { ExtractTablesWithRelations } from "drizzle-orm";
 
 type C_ENV = Pick<ENV, "DATABASE_URL_RAILWAY_POSTGRES" | "LOG_LEVEL">;
 
 export type Db = NodePgDatabase<typeof schema>;
+export type Tx = PgTransaction<
+  NodePgQueryResultHKT,
+  typeof schema,
+  ExtractTablesWithRelations<typeof schema>
+>;
 export class DrizzleClient {
   static instance: DrizzleClient | undefined;
   static getInstance = (ENV: C_ENV) => {
@@ -38,6 +49,26 @@ export class DrizzleClient {
     const { db, client } = await this.getDb();
     try {
       const result = await callback(db);
+      return result;
+    } catch (error: any) {
+      throw new Error(error.message);
+    } finally {
+      await this.endDb(client);
+    }
+  };
+
+  usingTrx = async <T>(callback: (tx: Tx) => Promise<T>) => {
+    const { db, client } = await this.getDb();
+    try {
+      const result = await db.transaction(async (tx) => {
+        try {
+          const result = await callback(tx);
+          return result;
+        } catch (error: any) {
+          tx.rollback();
+          throw new Error(error.message);
+        }
+      });
       return result;
     } catch (error: any) {
       throw new Error(error.message);
