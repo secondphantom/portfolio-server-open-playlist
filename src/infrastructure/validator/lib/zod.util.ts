8eb70553ab89f0495b66afd8ffa867ad7fffe6f6
SecondPhantom <second2phantom@gmail.com>
2024-05-22 09:18:59 +0900

    fix: zod boolean tarnsform

--- src/infrastructure/validator/lib/zod.util.ts
+++ src/infrastructure/validator/lib/zod.util.ts
@@ -2,4 +2,8 @@ import z from "zod";
 
 export const zodDateTransform = z.string().pipe(z.coerce.date());
 export const zodIntTransform = z.string().pipe(z.coerce.number());
-export const zodBooleanTransform = z.string().pipe(z.coerce.boolean());
+export const zodBooleanTransform = z
+  .string()
+  .toLowerCase()
+  .transform((x) => x === "true")
+  .pipe(z.boolean());
